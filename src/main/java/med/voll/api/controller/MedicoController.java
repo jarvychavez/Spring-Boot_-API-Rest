fc439642bf99bcf177c56c1c365720cf97bf2142
jarvychavez <jav.chavez94@gmail.com>
2025-06-30 12:44:49 -0600

    records de registros de las especialidades y la direccion

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DatosRegistroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ public class MedicoController {
 
 
     @PostMapping
-    public void registrar(@RequestBody String json){
-        System.out.println(json);
+    public void registrar(@RequestBody DatosRegistroMedico datos){
+        System.out.println(datos);
     }
 }
---
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -0,0 +1,6 @@
+package med.voll.api.medico;
+
+import med.voll.api.direccion.Direccion;
+
+public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad, Direccion direccion) {
+}
