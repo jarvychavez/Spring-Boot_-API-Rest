a44ee9beedac209b25b9a8d61e90c8b1554d6b30
jarvychavez <jav.chavez94@gmail.com>
2025-07-04 10:28:36 -0600

    pacientes,clases y metodos y agregar responseentity para que nos de un 204 en vez de 200 ok

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,26 +23,31 @@ public class MedicoController {
     @Transactional
     @PostMapping
     public void registrar(@RequestBody @Valid DatosRegistroMedico datos){
-
         repository.save(new Medico(datos));
     }
 
     @GetMapping
-    public Page<DatosListaMedico> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion){
-      return repository.findAllByActivoTrue(paginacion).map(DatosListaMedico::new);
+    public ResponseEntity<Page<DatosListaMedico>> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion){
+      var page = repository.findAllByActivoTrue(paginacion).map(DatosListaMedico::new);
+
+      return ResponseEntity.ok(page);
     }
 
     @Transactional
     @PutMapping
-    public void actualizar(@RequestBody @Valid DatosActualizacionMedico datos){
+    public ResponseEntity actualizar(@RequestBody @Valid DatosActualizacionMedico datos){
         var medico = repository.getReferenceById(datos.id());
         medico.actualizarInformaciones(datos);
+
+        return ResponseEntity.ok(new DatosDetalleMedico(medico));
     }
 
     @Transactional
     @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable Long id) {
+    public ResponseEntity eliminar(@PathVariable Long id) {
        var medico = repository.getReferenceById(id);
        medico.eliminar();
+
+       return ResponseEntity.noContent().build();
     }
 }
