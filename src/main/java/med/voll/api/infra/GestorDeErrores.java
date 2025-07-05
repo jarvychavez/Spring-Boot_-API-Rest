d7429b628c06474cfc25e3019f8859848c632245
jarvychavez <jav.chavez94@gmail.com>
2025-07-05 11:07:49 -0600

    errores de validacion

--- src/main/java/med/voll/api/infra/GestorDeErrores.java
+++ src/main/java/med/voll/api/infra/GestorDeErrores.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,4 +16,15 @@ public class GestorDeErrores {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity gestionarError400(MethodArgumentNotValidException ex) {
+        var errores = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValidacion::new).toList());
+    }
+
+    public record  DatosErrorValidacion(String campo, String mensaje){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 }
