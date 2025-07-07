b10368e8c3868481e4f9728841e014d2746de19c
jarvychavez <jav.chavez94@gmail.com>
2025-07-06 23:27:08 -0600

    pusimos el token en insomnia en la parte de Auth y lo dejamos en enable

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -14,6 +14,17 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
+
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null){
+            throw new RuntimeException("Token JWT no enviado en el encabezado de Authorization");
+        }
+        return authorizationHeader.replace("Bearer ","") ;
     }
 }
